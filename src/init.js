a3a0e795842ebee35e39ca5e6ee88ecd959ca698
Justin Cheng <jucheng.ca@gmail.com>
2022-11-17 12:56:13 -0800

    Complete pseudoclassical refactor

--- src/init.js
+++ src/init.js
@@ -15,16 +15,20 @@ $(document).ready(function() {
      * A new object of the given type will be created and added
      * to the stage.
      */
-    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+    //debugger
+    //
+
+
+    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name'); //makeBlickyDancer string
 
     // get the maker function for the kind of dancer we're supposed to make
-    var dancerMakerFunction = window[dancerMakerFunctionName];
+    var dancerMakerFunction = window[dancerMakerFunctionName]; // Getting the makeBlickyDancer Function
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+    var dancer = new dancerMakerFunction(
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
